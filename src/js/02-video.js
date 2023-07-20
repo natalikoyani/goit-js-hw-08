c94fd8f3dc79977964af2d292ea1ed95524417dd
natalikoyani <natalikagami@gmail.com>
2023-07-21 00:28:34 +0200

    7

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Player from '@vimeo/player';
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 
@@ -8,22 +8,11 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle((timeupdateHandler), 1000));
 
 function timeupdateHandler({ seconds }) {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds))
+    localStorage.setItem('videoplayer-current-time', seconds);
 }
 
 const savedCurrentTime = localStorage.getItem('videoplayer-current-time');
-const parsedCurrentTime = JSON.parse(savedCurrentTime);
 
-player.setCurrentTime(parsedCurrentTime).then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function (error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+if(savedCurrentTime) {
+    player.setCurrentTime(savedCurrentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,50 @@
+// import throttle from 'lodash.throttle';
+
+// const form = document.querySelector('.feedback-form');
+
+// const LOCALSTORAGE_KEY = 'feedback-form-state';
+// const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+// const parsedData = JSON.parse(savedData);
+// const formEmail = form.elements.email;
+// const formMessage = form.elements.message;
+
+
+// updateInputs();
+
+// form.addEventListener('input', throttle(saveData, 500));
+// form.addEventListener('submit', onSubmit);
+
+// function saveData() {
+//     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({email: formEmail.value, message: formMessage.value}));
+// }
+
+// function onSubmit(e) {
+//     e.preventDefault();
+//     console.log({email: formEmail.value, message: formMessage.value});
+//     localStorage.removeItem(LOCALSTORAGE_KEY);
+//     form.reset();
+// }
+
+// function updateInputs() {
+//     if(parsedData === null) {
+//         formEmail.value = "";
+//         formMessage.value = "";
+//     } else {
+//         formEmail.value = parsedData.email;
+//         formMessage.value = parsedData.message;
+//     }  
+// }
+
+// -------------------------------------------------------------------
+
+const throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
 const parsedData = JSON.parse(savedData);
 const formEmail = form.elements.email;
 const formMessage = form.elements.message;
-const throttle = require('lodash.throttle');
+
 
 
 updateInputs();
